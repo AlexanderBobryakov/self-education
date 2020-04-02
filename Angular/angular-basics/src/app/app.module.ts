9786aefa6335e2f7feeef94ef260308120e9eab1
AlexanderBobryakov <36169963+alexanderbobryakov@users.noreply.github.com>
2020-04-02 15:24:06 +0300

    change post4 component

--- Angular/angular-basics/src/app/app.module.ts
+++ Angular/angular-basics/src/app/app.module.ts
@@ -2,9 +2,9 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {PostComponent} from "./post/post.component";
+import {PostComponent} from './post/post.component';
 import {Post2Component} from './post2/post2.component';
-import {Post4Component} from "./post4/post4.component";
+import {Post4Component} from './post4/post4.component';
 
 @NgModule({
   declarations: [
--- Angular/angular-basics/src/app/post4/post4.component.ts
+++ Angular/angular-basics/src/app/post4/post4.component.ts
@@ -1,12 +1,18 @@
-import {Component} from "@angular/core";
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-post4',
   template: `
-    <div>
+    <div class="post4">
       TEMPLATE
     </div>
-  `
+  `,
+  styles: [`
+    .post4 {
+      padding: 50px 25px;
+      color: red;
+    }
+  `]
 })
 export class Post4Component {
 
