370533b9666b72942586ea75627f14ca5ff49c4f
AlexanderBobryakov <36169963+alexanderbobryakov@users.noreply.github.com>
2020-04-02 17:05:43 +0300

    directives

--- Angular/angular-basics/src/app/post2/post2.component.ts
+++ Angular/angular-basics/src/app/post2/post2.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-post2',
@@ -7,4 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Post2Component {
   backgroundToggle = false;
+  array = [1, 3, 4, 5, 6];
+  now = new Date();
 }
