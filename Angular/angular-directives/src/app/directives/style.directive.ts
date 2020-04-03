fc8023e5a4eb4c96117b79f1b34fbf2842395b6e
AlexanderBobryakov <36169963+alexanderbobryakov@users.noreply.github.com>
2020-04-03 12:55:19 +0300

    style-directive logic

--- Angular/angular-directives/src/app/directives/style.directive.ts
+++ Angular/angular-directives/src/app/directives/style.directive.ts
@@ -1,8 +1,22 @@
-import {Directive} from "@angular/core";
+import {Directive, ElementRef, HostListener, Renderer2} from "@angular/core";
 
 @Directive({
   selector: '[appStyle]'  // [] - чтобы это превратилось в атрибут
 })
 export class StyleDirective {
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {
+    this.renderer.setStyle(elementRef.nativeElement, 'color', 'green');
+  }
 
+  @HostListener('click', ['$event']) onCLick(event: Event) {
+    console.log(event)
+  }
+
+  @HostListener('mouseenter') onEnter() {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'blue')
+  }
+
+  @HostListener('mouseleave') onLeave() {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'red')
+  }
 }
