95220d5f1770bfed7a80e21429a011092bd09c7d
AlexanderBobryakov <36169963+alexanderbobryakov@users.noreply.github.com>
2020-04-05 17:08:16 +0300

    rxjs observable and Subject

--- Angular/angular-rxjs/src/app/app.component.ts
+++ Angular/angular-rxjs/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {interval, Subscription} from 'rxjs'
+import {interval, Observable, Subject, Subscription} from 'rxjs'
 import {map, filter, switchMap} from 'rxjs/operators'
 
 @Component({
@@ -10,6 +10,7 @@ import {map, filter, switchMap} from 'rxjs/operators'
 export class AppComponent {
 
   subscription: Subscription; // результат работы метода subscribe
+  stream$: Subject<number> = new Subject<number>();
 
   constructor() {
     const intervalStream$ = interval(1000); // вернет стрим
@@ -23,10 +24,36 @@ export class AppComponent {
       .subscribe((value) => {
         // value хранит каждую секунду новое значение = 0, 1, 2, 3...
       });
+
+
+    // СВОЙ СТРИМ через Observable
+    const stream$ = new Observable(observer => { // observer
+      observer.next(1);
+      observer.error('ERROR');
+      observer.complete(); // Завершает стрим
+    });
+    this.subscription = stream$
+      .subscribe(
+        value => {console.log(value)},
+        error => {console.log(error)},
+        () => {console.log("COMPLETE")}
+
+        );
+    this.subscription.unsubscribe();
+
+    // СВОЙ СТРИМ через Subject
+    this.subscription = this.stream$.subscribe(value => {
+      console.log(value);
+    })
+
   }
 
   stop() {
     // отписываемся от события
     this.subscription.unsubscribe();
+  }
+
+  next() {
+    this.stream$.next(Math.random())
   }
 }
