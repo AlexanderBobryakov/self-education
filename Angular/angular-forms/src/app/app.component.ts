963a569f8bafc95ac4b2c89dbe6295d686f165ec
AlexanderBobryakov <36169963+alexanderbobryakov@users.noreply.github.com>
2020-04-05 18:55:55 +0300

    form attribute binding

--- Angular/angular-forms/src/app/app.component.ts
+++ Angular/angular-forms/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-forms';
+export class AppComponent implements OnInit {
+  form: FormGroup;
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl('startValue'),
+      password: new FormControl(null),
+    });
+  }
+
+  // метод по отправке формы
+  submit() {
+    console.log(this.form)
+  }
 }
