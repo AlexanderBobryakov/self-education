5fb7aa4b9dd32c083c1c17023c2faf0196630da7
AlexanderBobryakov <36169963+alexanderbobryakov@users.noreply.github.com>
2020-04-05 16:49:34 +0300

    rxjs streamsa

--- Angular/angular-rxjs/src/app/app.component.ts
+++ Angular/angular-rxjs/src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-import {interval} from 'rxjs'
-
+import {Component} from '@angular/core';
+import {interval, Subscription} from 'rxjs'
+import {map, filter, switchMap} from 'rxjs/operators'
 
 @Component({
   selector: 'app-root',
@@ -9,11 +9,24 @@ import {interval} from 'rxjs'
 })
 export class AppComponent {
 
+  subscription: Subscription; // результат работы метода subscribe
+
   constructor() {
     const intervalStream$ = interval(1000); // вернет стрим
-    intervalStream$.subscribe((value) => {
-      // value хранит каждую секунду новое значение = 0, 1, 2, 3...
-    })
+    this.subscription = intervalStream$
+      // pipe позволяет применять несколько операторов к стриму
+      .pipe(
+        filter(value => value % 2 === 0),
+        map((value => `Mapped Value ${value}`)),
+      )
+      // подписываемся на событие
+      .subscribe((value) => {
+        // value хранит каждую секунду новое значение = 0, 1, 2, 3...
+      });
   }
 
+  stop() {
+    // отписываемся от события
+    this.subscription.unsubscribe();
+  }
 }
