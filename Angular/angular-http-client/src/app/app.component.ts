a11c93fb2339ee03d7300470631dc4d6b3e3c1ad
AlexanderBobryakov <36169963+alexanderbobryakov@users.noreply.github.com>
2020-04-06 20:14:08 +0300

    get/post example

--- Angular/angular-http-client/src/app/app.component.ts
+++ Angular/angular-http-client/src/app/app.component.ts
@@ -1,6 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
+export interface Todo {
+  completed: boolean,
+  title: string,
+  id?: number,
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,9 +14,32 @@ import {HttpClient} from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
 
+  todos: Todo[] = [];
+  todoTitle: string = '';
+
   constructor(private http: HttpClient) {
   }
 
   ngOnInit(): void {
+    this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
+      .subscribe(response => {
+        this.todos = response;
+      })
+  }
+
+  addTodo() {
+    if (!this.todoTitle.trim()) {
+      return;
+    }
+    const newTodo: Todo = {
+      title: this.todoTitle,
+      completed: false,
+    };
+    this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', newTodo)
+      .subscribe(response => {
+        console.log(response);
+        this.todos.unshift(response);
+        this.todoTitle = '';
+      })
   }
 }
