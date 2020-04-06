390e6b5a8e1b5fc1400deb1f27bc88b2efb007e6
AlexanderBobryakov <36169963+alexanderbobryakov@users.noreply.github.com>
2020-04-06 23:55:38 +0300

    observe

--- Angular/angular-http-client/src/app/todo.service.ts
+++ Angular/angular-http-client/src/app/todo.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from "@angular/core";
-import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
+import {HttpClient, HttpEventType, HttpHeaders, HttpParams, HttpResponse} from "@angular/common/http";
 import {Observable, throwError} from "rxjs";
-import {catchError, delay} from "rxjs/operators";
+import {catchError, delay, map, tap} from "rxjs/operators";
 
 export interface Todo {
   completed: boolean,
@@ -28,9 +28,14 @@ export class TodoService {
   fetchTodos(): Observable<Todo[]> {
     return this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?', {
       // params: new HttpParams().set('_limit', '3')
-      params: new HttpParams().append('_limit', '4').append('custom', 'wwrewe')
+      params: new HttpParams().append('_limit', '4').append('custom', 'wwrewe'),
+      observe: "response"  // observe - То что хотим получать из ответа
+                      // body - по умолчанию получаем сам body
+                      // response - получаем весь resposnse
+                      // 'events' - получаем доступ ко всем событиям в асинхронном запросе на всех этапах
     })
       .pipe(
+        map(response => response.body),
         delay(500),
         catchError(err => {
           console.log(err);
@@ -39,13 +44,25 @@ export class TodoService {
       );
   }
 
-  deleteTodo(id: number): Observable<void> {
-    return this.http.delete<void>(`https://jsonplaceholder.typicode.com/todos/${id}`);
+  deleteTodo(id: number): Observable<any> {
+    return this.http.delete<void>(`https://jsonplaceholder.typicode.com/todos/${id}`,
+      {
+        observe: 'events'  // получаем доступ ко всем событиям в асинхронном запросе на всех этапах
+      }).pipe(
+        tap(event => {
+          console.log(event);
+          if (event.type === HttpEventType.Response) {
+            console.log('response event', event);
+          }
+        })
+    );
   }
 
   competeTodo(id: number):Observable<Todo> {
     return this.http.put<Todo>(`https://jsonplaceholder.typicode.com/todos/${id}`, {
-      completed: true
+      completed: true,
+    }, {
+      responseType: "json"
     });
   }
 }
