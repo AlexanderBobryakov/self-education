10b7ede50484f5deea8d93cb7ef077eed843f2c2
AlexanderBobryakov <36169963+alexanderbobryakov@users.noreply.github.com>
2020-04-08 02:08:55 +0300

    seo + tags

--- Angular/angular-additional/src/app/app.component.ts
+++ Angular/angular-additional/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, ComponentFactoryResolver, ViewChild} from '@angular/core'
 import {ModalComponent} from "./modal/modal.component";
 import {RefDirective} from "./ref.directive";
+import {Meta, Title} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,12 @@ export class AppComponent {
 
   @ViewChild(RefDirective) refDirective: RefDirective;
 
-  constructor(private resolver: ComponentFactoryResolver) {
+  constructor(private resolver: ComponentFactoryResolver,
+              private title: Title,
+              private meta: Meta
+  ) {
+    const t = title.getTitle(); // title приложения angular-additional
+    meta.addTags([{name: ' keywords', content: ' angular, google, app'}]); // добавляем мета информацию в meta тэги
   }
 
   // показываем динамически модальное окно
