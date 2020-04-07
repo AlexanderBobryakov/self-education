967fe74eb1056e311bb93cf09e912e8413d18589
AlexanderBobryakov <36169963+alexanderbobryakov@users.noreply.github.com>
2020-04-07 03:27:49 +0300

    404 error

--- Angular/angular-routing/src/app/app-routing.module.ts
+++ Angular/angular-routing/src/app/app-routing.module.ts
@@ -4,12 +4,20 @@ import {HomeComponent} from "./home/home.component";
 import {AboutComponent} from "./about/about.component";
 import {PostsComponent} from "./posts/posts.component";
 import {PostComponent} from "./post/post.component";
+import {AboutExtraComponent} from "./about-extra/about-extra.component";
+import {ErrorPageComponent} from "./error-page/error-page.component";
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'about', component: AboutComponent},
+  {
+    path: 'about', component: AboutComponent, children: [
+      {path: 'extra', component: AboutExtraComponent},
+    ]
+  },
   {path: 'posts', component: PostsComponent},
   {path: 'posts/:id', component: PostComponent},
+  {path: 'error', component: ErrorPageComponent},
+  {path: '**', redirectTo: '/error'},  // всегда последний для всех остальных роутов
 ];
 
 @NgModule({
--- Angular/angular-routing/src/app/app.module.ts
+++ Angular/angular-routing/src/app/app.module.ts
@@ -9,6 +9,7 @@ import {PostsComponent} from './posts/posts.component'
 import {PostComponent} from './post/post.component'
 import {AboutExtraComponent} from './about-extra/about-extra.component'
 import {AppRoutingModule} from "./app-routing.module";
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import {AppRoutingModule} from "./app-routing.module";
     PostsComponent,
     PostComponent,
     AboutExtraComponent,
+    ErrorPageComponent,
   ],
   imports: [
     BrowserModule,
