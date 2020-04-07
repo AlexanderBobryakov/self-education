ad8e21bf806207a126d69fadfa857da13aa6614f
AlexanderBobryakov <36169963+alexanderbobryakov@users.noreply.github.com>
2020-04-08 01:16:42 +0300

    preloadingStrategy

--- Angular/angular-modules/src/app/app-routing.module.ts
+++ Angular/angular-modules/src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import {NgModule} from '@angular/core'
-import {RouterModule} from '@angular/router'
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router'
 import {HomePageComponent} from './home-page/home-page.component'
 
+const routes: Routes = [
+  {path: '', component: HomePageComponent, pathMatch: 'full'},
+  {path: 'about', loadChildren: './about-page/about-page.module#AboutPageModule'},  // ленивая загрузка модуля - старый вариант
+  {path: 'about', loadChildren: () => import('./about-page/about-page.module').then(module => module.AboutPageModule)},  // ленивая загрузка модуля - новый вариант
+];
+
 @NgModule({
-  imports: [RouterModule.forRoot([
-    {path: '', component: HomePageComponent, pathMatch: 'full'},
-    {path: 'about', loadChildren: './about-page/about-page.module#AboutPageModule'},  // ленивая загрузка модуля - старый вариант
-    {path: 'about', loadChildren: () => import('./about-page/about-page.module').then(module => module.AboutPageModule)},  // ленивая загрузка модуля - новый вариант
-  ])],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules  // стратегия загрузки модулей (сначала все необходимое для страницы, а потом в фоне все модули)
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
